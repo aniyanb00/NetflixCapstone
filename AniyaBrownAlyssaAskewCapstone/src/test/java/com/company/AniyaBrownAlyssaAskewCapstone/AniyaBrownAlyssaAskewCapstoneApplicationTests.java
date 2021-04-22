5db17e2483736367608739dae88f352f038fb2e2
aniya <aniyanb00@gmail.com>
2021-04-22 19:49:58 -0400

    added folder -spring boot

---
+++ AniyaBrownAlyssaAskewCapstone/src/test/java/com/company/AniyaBrownAlyssaAskewCapstone/AniyaBrownAlyssaAskewCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.AniyaBrownAlyssaAskewCapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AniyaBrownAlyssaAskewCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
