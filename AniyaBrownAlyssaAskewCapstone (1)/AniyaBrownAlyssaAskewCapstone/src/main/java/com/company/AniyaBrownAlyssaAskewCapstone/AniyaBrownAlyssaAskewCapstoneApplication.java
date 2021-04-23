9ccce0b5418dfb2230d31a82fb0f5ba42b44b644
aniya <aniyanb00@gmail.com>
2021-04-22 21:24:27 -0400

    new project

---
+++ AniyaBrownAlyssaAskewCapstone (1)/AniyaBrownAlyssaAskewCapstone/src/main/java/com/company/AniyaBrownAlyssaAskewCapstone/AniyaBrownAlyssaAskewCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.company.AniyaBrownAlyssaAskewCapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AniyaBrownAlyssaAskewCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AniyaBrownAlyssaAskewCapstoneApplication.class, args);
+	}
+
+}
